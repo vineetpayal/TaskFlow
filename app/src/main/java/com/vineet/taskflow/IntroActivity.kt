76d06e264b6b440781b9f8115a5daa0d7307aa61
Vineet <vineet.pyl@gmail.com>
2024-09-21 23:08:43 +0530

    Implemented IntroActivity

--- app/src/main/java/com/vineet/taskflow/IntroActivity.kt
+++ app/src/main/java/com/vineet/taskflow/IntroActivity.kt
@@ -1,29 +1,20 @@
 package com.vineet.taskflow
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SplashActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_intro)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        Handler().postDelayed({
-            startActivity(Intent(this,IntroActivity::class.java))
-             finish()
-        },2500)
-
-
     }
 }
