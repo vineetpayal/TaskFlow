69c6f86f493383303e013c2b6013db3dd9f0baff
Vineet <vineet.pyl@gmail.com>
2024-09-22 22:00:21 +0530

    Implemented RegisterActivity Ui. To fix: edit text layout in ui

--- app/src/main/java/com/vineet/taskflow/RegisterActivity.kt
+++ app/src/main/java/com/vineet/taskflow/RegisterActivity.kt
@@ -1,17 +1,16 @@
 package com.vineet.taskflow
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class IntroActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_intro)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
