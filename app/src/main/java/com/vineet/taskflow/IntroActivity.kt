bfee77d87c2b87d6bf7062520c1224cf4313935f
vineetpayal <vineet.pyl@gmail.com>
2024-11-08 09:51:40 +0530

    ubuntu import

--- app/src/main/java/com/vineet/taskflow/IntroActivity.kt
+++ app/src/main/java/com/vineet/taskflow/IntroActivity.kt
@@ -2,12 +2,16 @@ package com.vineet.taskflow
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class IntroActivity : AppCompatActivity() {
+
+     lateinit var btnRegister : Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +21,13 @@ class IntroActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btnRegister = findViewById(R.id.btn_register);
+
+        btnRegister.setOnClickListener {
+            startActivity(Intent(this,RegisterActivity::class.java))
+            finish();
+        }
+
     }
 }
